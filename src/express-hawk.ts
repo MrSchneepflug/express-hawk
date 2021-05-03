e2c2261b0ee5900236d667f80cd67443827b3282
Arne Schubert <arne.schubert@rewe-digital.com>
2021-05-03 15:36:10 +0200

    Add a possibility to change the bot-detection list
    write tests

--- src/express-hawk.ts
+++ src/express-hawk.ts
@@ -5,16 +5,16 @@ export interface HawkRequest extends Request {
     isBot?: boolean;
 }
 
-function hawk() {
-    const blacklistContent = fs.readFileSync(__dirname + "/../user-agent.blacklist", {encoding: "utf8"});
-    const blacklist = blacklistContent.trim().split("\n");
-    const blacklistRegex = new RegExp(blacklist.join("|"));
+function hawk(blacklistContent = predefinedBlacklist) {
+    const blacklistRegex = new RegExp(blacklistContent.trim().split("\n").join("|"));
 
     return (req: HawkRequest, res: Response, next: NextFunction) => {
         // The user-agent could be undefined. This is fine since the test-method will return false for undefined.
-        req.isBot = blacklistRegex.test(req.headers["user-agent"] as any);
+        req.isBot = blacklistRegex.test(req.headers["user-agent"] as string | undefined || "");
         next();
     };
 }
+
+export const predefinedBlacklist = fs.readFileSync(__dirname + "/../user-agent.blacklist", {encoding: "utf8"});
 
 export {hawk};
--- test/express-hawk.spec.ts
+++ test/express-hawk.spec.ts
@@ -1,7 +1,8 @@
 import chai, {expect} from "chai";
 import chaiHttp from "chai-http";
 import express, {Response} from "express";
-import {hawk, HawkRequest} from "../src/express-hawk";
+import fs from "fs";
+import {hawk, HawkRequest, predefinedBotlist} from "../src/express-hawk";
 
 chai.use(chaiHttp);
 
@@ -25,7 +26,7 @@ describe("express-hawk is an express-middleware", () => {
         app.get("/match-test", (req: HawkRequest, res: Response) => {
             res.json({
                 isBot: req.isBot
-            })
+            });
         });
 
         it("will set 'isBot' to false if the user-agent does not match", (done) => {
@@ -49,5 +50,37 @@ describe("express-hawk is an express-middleware", () => {
                     done();
                 });
         });
+    });
+
+    describe("it will be possible to pass your own botlists", () => {
+        const ownListApp = express();
+        ownListApp.use(hawk("my-own-defined-bot"));
+        ownListApp.get("/match-test", (req: HawkRequest, res: Response) => {
+            res.json({
+                isBot: req.isBot
+            });
+        });
+
+        it("should provide the bot-list as export", () => expect(predefinedBotlist)
+            .eq(fs.readFileSync(__dirname + "/../user-agent.botlist", {encoding: "utf8"})));
+
+        it("should find mentioned bots", (done) => void chai.request(ownListApp)
+                .get("/match-test")
+                .set("user-agent", "my-own-defined-bot")
+                .send()
+                .end((err, res) => {
+                    expect(res.body.isBot).to.be.true;
+                    done();
+                })
+        );
+        it("should not find bots that were not mentioned", (done) => void chai.request(ownListApp)
+            .get("/match-test")
+            .set("user-agent", "my-not-defined-bot")
+            .send()
+            .end((err, res) => {
+                expect(res.body.isBot).to.be.false;
+                done();
+            })
+        );
     });
 });
